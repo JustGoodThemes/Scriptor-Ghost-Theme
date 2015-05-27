e6b1f79754cd995e192d1555111a4f7a20879279
Tomas <tbdizainas@gmail.com>
2015-05-27 19:28:18 +0300

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+/**
+ * Main JS file for Scriptor behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+    });
+
+}(jQuery));
