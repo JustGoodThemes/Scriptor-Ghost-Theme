d38faf684699edcbb3a22ac270b0f43cdd3e71c4
astab <astabankauske@gmail.com>
2018-09-18 22:05:33 +0300

    Ghost 2.1.2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,7 +9,17 @@
 	$(document).ready(function(){
 
 		// Responsive video embeds
-		$(".post-content").fitVids();
+		$('.post-content').fitVids();
+
+		// Gallery adjustments
+		$( '.kg-gallery-image > img' ).each( function() {
+			var _this = $(this),
+				$container = _this.closest('.kg-gallery-image'),
+				width = _this.attr('width'),
+				height = _this.attr('height'),
+				ratio = width / height;
+			$container.css({'flex' : ratio + ' 1 0%' });
+		});
 
 		// Scroll to top
 		$('#back-to-top').on('click', function(e) {
